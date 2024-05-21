e79ba309e48835170652dc1d728ad11359c1c260
rajak <rajak@RajaBalaji>
2024-05-21 15:47:51 +0530

    fork learn commit

--- SeleniumCRMGUIFramework/src/test/java/practicegit/ToPracticeForkAndPullRequest.java
+++ SeleniumCRMGUIFramework/src/test/java/practicegit/ToPracticeForkAndPullRequest.java
@@ -3,6 +3,7 @@ package practicegit;
 public class ToPracticeForkAndPullRequest {
 	public static void main(String[] args) {
 		System.out.println("fork and pull request done");
+		System.out.println("git learn");
 	}
 
 }
