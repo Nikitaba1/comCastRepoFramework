8a0ef357856db163f94f8da9eb81b0595a86b734
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-05-21 10:20:14 +0530

    separate product page is added

---
+++ SeleniumCRMGUIFramework/src/main/java/com/Vtiger/Pomclass/ProductPage.java
@@ -0,0 +1,26 @@
+package com.Vtiger.Pomclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+	
+	public ProductPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(linkText = "Products")
+	private WebElement productlink;
+
+
+
+
+	public WebElement getProductlink() {
+		return productlink;
+	}
+
+
+
+}
